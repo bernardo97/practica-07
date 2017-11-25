3965c9a8c988a9add49def37ab17f41d0af07df6
Bernardo Galvan <32716213+bernardo97@users.noreply.github.com>
2017-11-25 16:46:14 -0600

    Create funcionesblack.h

---
+++ funcionesblack.h
@@ -0,0 +1,11 @@
+#ifndef funcionesblack_h
+#define funcionesblack_h
+
+
+#include <stdio.h>
+ 	void bienvenida();
+    void ayuda();
+    void cartas(char otra,int puntos,int punt,int *vect,int carta);
+    void puntaje();
+    
+#endif
